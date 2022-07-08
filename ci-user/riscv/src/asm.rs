587d92ae7d450bf0cacd6e5fc4427691f53792a2
aucker <aucker@qq.com>
2022-07-08 10:44:07 +0800

    import core::arch::asm in two files

--- ci-user/riscv/src/asm.rs
+++ ci-user/riscv/src/asm.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 //! Assembly instructions
 macro_rules! instruction {
     ($(#[$attr:meta])*, $fnname:ident, $asm:expr, $asm_fn:ident) => (
--- ci-user/riscv/src/register/macros.rs
+++ ci-user/riscv/src/register/macros.rs
@@ -1,3 +1,4 @@
+use core::arch::asm;
 macro_rules! read_csr {
     ($csr_number:expr, $asm_fn: ident) => {
         /// Reads the CSR
